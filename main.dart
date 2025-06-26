3a0852ac1b381e490ea0f4bd443b139de8b3b1f7
Brokker500 <ayhanaycan1@gmail.com>
2025-06-26 23:24:50 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,49 +1,11 @@
+
 import 'package:flutter/material.dart';
-import 'map_screen.dart';
-import 'driver_map.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Akıllı Durak',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: RoleSelectionScreen(),
-    );
-  }
-}
-
-class RoleSelectionScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Rol Seçimi')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => MapScreen()),
-              ),
-              child: Text('Yolcu olarak devam et'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => DriverMapScreen()),
-              ),
-              child: Text('Sürücü olarak devam et'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(home: Scaffold(body: Center(child: Text('Akilli Durak'))));
   }
 }
